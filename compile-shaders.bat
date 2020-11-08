ae41d4e1e5d089755214713fd92dc6f730ddf094
Tom Hulton-Harrop <tom.hultonharrop@gmail.com>
2020-11-08 22:58:30 +0000

    fix path for compiler-shader scripts

--- compile-shaders.bat
+++ compile-shaders.bat
@@ -4,9 +4,9 @@ REM compile shaders
 
 REM simple shader
 third-party\build\bin\shaderc.exe ^
--f shader\simple\v_simple.sc -o shader\simple\v_simple.bin ^
+-f shader\v_simple.sc -o shader\v_simple.bin ^
 --platform windows --type vertex --verbose -i ./ -p vs_5_0
 
 third-party\build\bin\shaderc.exe ^
--f shader\simple\f_simple.sc -o shader\simple\f_simple.bin ^
+-f shader\f_simple.sc -o shader\f_simple.bin ^
 --platform windows --type fragment --verbose -i ./ -p ps_5_0
--- compile-shaders.sh
+++ compile-shaders.sh
@@ -4,9 +4,9 @@
 
 # simple shader
 ./third-party/build/bin/shaderc  \
--f shader/simple/v_simple.sc -o shader/simple/v_simple.bin  \
+-f shader/v_simple.sc -o shader/v_simple.bin  \
 --platform osx --type vertex --verbose -i ./ -p metal
 
 ./third-party/build/bin/shaderc \
--f shader/simple/f_simple.sc -o shader/simple/f_simple.bin \
+-f shader/f_simple.sc -o shader/f_simple.bin \
 --platform osx --type fragment --verbose -i ./ -p metal
