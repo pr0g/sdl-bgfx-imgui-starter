8c591893a8b3f299692ad21700044971554b2dc8
Tom Hulton-Harrop <tom.hultonharrop@gmail.com>
2020-11-09 22:01:41 +0000

    update configure to use Release

--- configure.sh
+++ configure.sh
@@ -6,6 +6,6 @@ cmake -S . -B build/debug -G Ninja -DCMAKE_BUILD_TYPE=Debug \
 -DCMAKE_PREFIX_PATH="$(pwd)/third-party/build" \
 -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
 
-cmake -S . -B build/release -G Ninja -DCMAKE_BUILD_TYPE=RelWithDebInfo \
+cmake -S . -B build/release -G Ninja -DCMAKE_BUILD_TYPE=Release \
 -DCMAKE_PREFIX_PATH="$(pwd)/third-party/build" \
 -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
