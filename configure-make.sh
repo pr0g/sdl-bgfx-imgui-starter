bdb88d6ba560a25f77f6ff5bff8215b0a2241b46
Tom Hulton-Harrop <tom.hultonharrop@gmail.com>
2021-09-05 15:39:29 +0100

    update configure scripts

--- configure-make.sh
+++ configure-make.sh
@@ -2,10 +2,10 @@
 
 # Commands to configure this repo for building after dependencies have been installed
 
-cmake -S . -B build/debug -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Debug \
+cmake -S . -B build/debug-make -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Debug \
 -DCMAKE_PREFIX_PATH="$(pwd)/third-party/build" \
 -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
 
-cmake -S . -B build/release -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release \
+cmake -S . -B build/release-make -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release \
 -DCMAKE_PREFIX_PATH="$(pwd)/third-party/build" \
 -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
--- configure-vs.bat
+++ configure-vs.bat
@@ -2,5 +2,5 @@
 
 REM Command to configure this repo for building after dependencies have been installed
 
-cmake -S . -B build-vs -G "Visual Studio 16 2019" -A x64 ^
+cmake -S . -B build/vs -G "Visual Studio 16 2019" -A x64 ^
 -DCMAKE_PREFIX_PATH=%cd%/third-party/build
