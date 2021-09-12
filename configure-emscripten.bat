f41e8f6dd65ae2b3e47ffc97b4397e5ce96abc2e
Tom Hulton-Harrop <tom.hultonharrop@gmail.com>
2021-09-12 18:25:46 +0100

    ensure multiple .bat invocations can happen for emcmake

--- configure-emscripten.bat
+++ configure-emscripten.bat
@@ -2,10 +2,10 @@
 
 REM Commands to configure this repo for building after dependencies have been installed
 
-emcmake cmake -S . -B build/debug-emscripten -G Ninja ^
+call emcmake cmake -S . -B build/debug-emscripten -G Ninja ^
 -DCMAKE_BUILD_TYPE=Debug ^
 -DCMAKE_PREFIX_PATH=%cd%/third-party/build
 
-emcmake cmake -S . -B build/release-emscripten -G Ninja ^
+call emcmake cmake -S . -B build/release-emscripten -G Ninja ^
 -DCMAKE_BUILD_TYPE=Release ^
 -DCMAKE_PREFIX_PATH=%cd%/third-party/build
