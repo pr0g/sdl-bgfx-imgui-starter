30fa73072a921afc4e7b4478efc938ab372066a5
Tom Hulton-Harrop <tom.hultonharrop@gmail.com>
2021-09-12 16:44:09 +0100

    fix for emscripten building on windows

--- compile-shaders-emscripten.sh
+++ compile-shaders-emscripten.sh
@@ -4,7 +4,7 @@
 
 # simple shader
 ./third-party/build/bin/shaderc \
--f shader/v_simple.sc -o shader/v_simple.bin  \
+-f shader/v_simple.sc -o shader/v_simple.bin \
 --platform asmjs --type vertex --verbose -i ./
 
 ./third-party/build/bin/shaderc \
