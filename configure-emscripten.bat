d9d19b47194015100e964472b692f293c4fe8a13
Tom Hulton-Harrop <tom.hultonharrop@gmail.com>
2021-09-12 18:05:26 +0100

    update configure-emscripten.bat file

--- configure-emscripten.bat
+++ configure-emscripten.bat
@@ -1,4 +1,11 @@
 @echo off
 
-emcmake cmake -S . -B build/emscripten -G Ninja ^
+REM Commands to configure this repo for building after dependencies have been installed
+
+emcmake cmake -S . -B build/debug-emscripten -G Ninja ^
+-DCMAKE_BUILD_TYPE=Debug ^
+-DCMAKE_PREFIX_PATH=%cd%/third-party/build
+
+emcmake cmake -S . -B build/release-emscripten -G Ninja ^
+-DCMAKE_BUILD_TYPE=Release ^
 -DCMAKE_PREFIX_PATH=%cd%/third-party/build
