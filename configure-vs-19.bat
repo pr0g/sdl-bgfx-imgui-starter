23a6fb570d71c07fd07de6530eeac6112de07a66
Tom Hulton-Harrop <tom.hultonharrop@gmail.com>
2022-02-07 18:37:31 +0000

    update configure files for visual studio

--- configure-vs-19.bat
+++ configure-vs-19.bat
@@ -3,5 +3,5 @@
 REM Command to configure this repo for building using the Visual Studio 2019
 REM generator (after dependencies have been installed)
 
-cmake -B build/vs19 -G "Visual Studio 16 2019" -A x64 ^
+cmake -B build/vs2019 -G "Visual Studio 16 2019" -A x64 ^
 -DCMAKE_PREFIX_PATH=%cd%/third-party/build
--- configure-vs-22.bat
+++ configure-vs-22.bat
@@ -3,5 +3,5 @@
 REM Command to configure this repo for building using the Visual Studio 2022
 REM generator (after dependencies have been installed)
 
-cmake -B build/vs19 -G "Visual Studio 17 2022" -A x64 ^
+cmake -B build/vs2022 -G "Visual Studio 17 2022" -A x64 ^
 -DCMAKE_PREFIX_PATH=%cd%/third-party/build
