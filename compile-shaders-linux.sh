a5da7a0fcc235be37a002b16f247377cd10bd6e1
Tom Hulton-Harrop <tom.hultonharrop@gmail.com>
2022-10-16 15:16:04 +0100

    update shader compilation scipt for linux

--- compile-shaders-linux.sh
+++ compile-shaders-linux.sh
@@ -5,8 +5,8 @@
 # simple shader
 ./third-party/build/bin/shaderc \
 -f shader/v_simple.sc -o shader/v_simple.bin  \
---platform linux --type vertex --verbose -i ./
+--platform linux --type vertex --verbose -i ./ -p spirv
 
 ./third-party/build/bin/shaderc \
 -f shader/f_simple.sc -o shader/f_simple.bin \
---platform linux --type fragment --verbose -i ./
+--platform linux --type fragment --verbose -i ./ -p spirv
