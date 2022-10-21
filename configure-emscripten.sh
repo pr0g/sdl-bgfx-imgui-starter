b0dfb386eae09553d4ee5881ed9a6265c8d8fd6d
Tom Hulton-Harrop <tom.hultonharrop@gmail.com>
2022-10-21 16:44:19 +0100

    updates for emscripten superbuild

--- configure-emscripten.sh
+++ configure-emscripten.sh
@@ -2,8 +2,8 @@
 
 # Commands to configure this repo for building after dependencies have been installed
 
-emcmake cmake -B build/debug-emscripten -DCMAKE_BUILD_TYPE=Debug \
+emcmake cmake -B build/debug-emscripten -G Ninja -DCMAKE_BUILD_TYPE=Debug \
 -DCMAKE_PREFIX_PATH="$(pwd)/third-party/build"
 
-emcmake cmake -B build/release-emscripten -DCMAKE_BUILD_TYPE=Release \
+emcmake cmake -B build/release-emscripten -G Ninja -DCMAKE_BUILD_TYPE=Release \
 -DCMAKE_PREFIX_PATH="$(pwd)/third-party/build"
